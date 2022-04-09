241052cb445dfe6dd076afe7d2947897338376d2
Agus <agustin.fourcade@externos-ar.cencosud.com>
2022-04-09 01:23:44 -0300

    update date

--- src/routes/health_route.js
+++ src/routes/health_route.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/health", (req, res) => {
-  res.send("Health OK! - Search Engine by Agustin Fourcade");
+  res.send("Health OK! - Search Engine by Agustin Fourcade April 2022");
 });
 
 module.exports = router;
