cf1b5178facf4516bcfb8a5280c2daa9d9fa6835
Agus <agustin.fourcade@externos-ar.cencosud.com>
2022-04-10 14:48:09 -0300

    product views on search response

--- src/controllers/search_controller.js
+++ src/controllers/search_controller.js
@@ -1,8 +1,13 @@
 const { performSearch } = require("../helpers/search_helper");
 const application_controller = require("./application_controller");
+const ProductViews = require("../models/product_view");
 
 const index = async (req) => {
-  const response = await performSearch(req.query);
+  const { view_name, text } = req.query;
+
+  const response = await performSearch({ text });
+
+  response.views = await ProductViews.views(response.all_product_ids, view_name);
 
   return { response };
 };
--- src/models/product_view.js
+++ src/models/product_view.js
@@ -21,4 +21,15 @@ const ProductViewSchema = new mongoose.Schema(
   }
 );
 
+ProductViewSchema.statics.views = async function (ids, name = "default") {
+  if (!ids) {
+    return [];
+  }
+  return (
+    await this.find({ product_id: { $in: ids }, name }, { _id: 0 }).exec()
+  )
+    .sort((a, b) => ids.indexOf(a.product_id) - ids.indexOf(b.product_id))
+    .map((e) => e.content);
+};
+
 module.exports = mongoose.model("ProductView", ProductViewSchema);
